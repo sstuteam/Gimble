ecfd5c54bb8d7da9609b9c93f504343527605794
Tsvetkov <zwetvik@gmail.com>
2016-01-16 14:56:45 +0300

    header и footer вынесены в единые скрипты для всех страниц

---
+++ Prototypes/Scripts/footer.js
@@ -0,0 +1,10 @@
+var html = `
+    <footer class="footer center-block">
+        <!--Название компании и год её основания-->
+        <div class="container">
+            <p class="text-muted">IBS Production &copy; 2015</p>
+        </div>
+    </footer>
+`;
+
+$( "body" ).append(html);
---
+++ Prototypes/Scripts/header.js
@@ -0,0 +1,25 @@
+var html = `
+  <header class="head-page row">
+            <!--logotype-->
+            <div class="logo_pull-left">
+                <a id="head-logo" href="index.html"></a>
+            </div>
+            <ul class="profile-button list-unstyled pull-right">
+                <li class="dropdown">
+                    <a class="drop-btn dropdown-toggle" data-toggle="dropdown">
+                        <span class="glyphicon glyphicon-user"></span>
+                        Имя пользователя
+                        <span class="glyphicon glyphicon-triangle-bottom"></span>
+                    </a>
+                    <ul class="dropdown-menu">
+                        <li><a href="profile.html">Мой профиль</a></li>
+                        <li><a href="settings.html">Настройки</a></li>
+                        <li class="divider"></li>
+                        <li><a href="#">Выйти из себя</a></li>
+                    </ul>
+                </li>
+            </ul>
+</header>
+`;
+
+$( ".container-fluid:first" ).prepend(html);
