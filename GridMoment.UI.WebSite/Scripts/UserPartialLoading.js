a29adaa94c7f2b668a49e1b9ed7a901c15e989ad
sardimka <sardimka96@mail.ru>
2016-04-24 11:48:56 +0400

    Исправлены некоторые ошибки. Добавлена подругзка постов при скроллинге ленты, гифка ожидания загрузки.

--- GridMoment.UI.WebSite/Scripts/UserPartialLoading.js
+++ GridMoment.UI.WebSite/Scripts/UserPartialLoading.js
@@ -1,28 +1,22 @@
-﻿/// <reference path="jquery-1.10.2.intellisense.js" />
-/// <reference path="jquery-2.2.0.js" />
-(function () {
-    $(document.body).on("click", "ajax-loading", function (event) {
-        var elem = $(this);
-        $.ajax({
-            url: elem.attr("href"),
-            data: null,
-            method: "get",
-            success: function (data) {
-                $("#main").empty().append(data);
-            }
-        })
-    })
-})();
-
-$(function () {
+﻿$(function () {
     var trying = false;
-    var partialPages = ["#last-story-panel", "#best-story-7", "#best-story-30"];
+    var partialPages = ["#panel1", "#panel2"];
     var currentIdPanel;
+    var url = "";
     for (var item in partialPages) {
         if ($(item).hasClass("active"));
         currentIdPanel = item;
     }
-    //подгрузка элементов
+    switch (item) {
+        case "#panel1":
+            url = "/Post/ShowUserNews/";
+            break;
+        case "#panel2":
+            url = "/Post/ShowUserBookmarks/";
+            break;
+        default:
+
+    }
     function loadItems() {
         $('div#loading').hide();
         if (!trying) {
@@ -30,7 +24,7 @@ $(function () {
             $('div#loading').show();
             $.ajax({ //ajax-запрос
                 type: 'GET',
-                url: '/Home/Index/' + currentIdPanel,
+                url: url + currentIdPanel,
                 success: function (data, status) {
                     if (data != '') {
                         $(currentIdPanel).append(data);
@@ -51,4 +45,4 @@ $(function () {
             loadItems();
         }
     });
-})
+})();
